fd6c99227550c6b46203164acf63d762b4ce2440
Ryan <ryan.lawrence.business@gmail.com>
2025-01-14 17:47:04 +1100

    Update streak_tracker_2.py

--- streak_tracker_2.py
+++ streak_tracker_2.py
@@ -34,6 +34,12 @@ CBOLD   = '\33[1m'
 
 CEND    = '\033[0m'
 
+def next_day(day):
+    return day + datetime.timedelta(days=1)
+
+def prev_day(day):
+    return day - datetime.timedelta(days=1)
+
 class Streak:
     def __init__(self, name, true_days = [], false_days = []):
         self.name = name
@@ -41,6 +47,9 @@ class Streak:
         self.false_days = false_days
 
     def add_today(self):
+        if self.day_is_present(TODAY):
+            print(f'Skipping "{self.name}", as it has already been tracked today.')
+            return
         print(f"Did you {self.name} today? (Y/N)")
         yes = input().capitalize() == "Y"
         if yes: self.true_days.append(TODAY)
@@ -50,8 +59,45 @@ class Streak:
         if day in self.true_days: print(CGREEN + SQUARE + CEND)
         elif day in self.false_days: print(CRED + SQUARE + CEND)
         else: print(SQUARE)
+
+    def num_of_true_days(self):
+        return len(self.true_days)
     
+    def count_streak(self, start):
+        "Counts streak length from a starting position (right to left)"
+        count = 0
+        pos = start
+        while pos > 0:
+            count += 1
+            if prev_day(self.true_days[pos]) != self.true_days[pos - 1]:
+                return count
+            pos -= 1
+
+    def find_longest_streak(this):
+        pos = len(this.true_days) - 1
+        largest = 0
+        while pos > 0:
+            pos_streak = this.count_streak(pos)
+            if pos_streak> largest:
+                largest = pos_streak
+            pos -= 1
+        return largest
+
+    def day_is_present(self, day):
+        if day in self.true_days or day in self.false_days: 
+            return True
+        else:
+            return False
+
 
 test = Streak("kill youtself")
 test.add_today()
-test.print_day(TODAY)
+test.add_today()
+test.true_days.append(prev_day(prev_day(prev_day(TODAY))))
+test.true_days.append(prev_day(prev_day(TODAY)))
+test.true_days.append(prev_day(TODAY))
+test.add_today()
+# test.print_day(TODAY)
+
+# print(test.count_streak(len(test.true_days) - 1))
+print(test.find_longest_streak())
